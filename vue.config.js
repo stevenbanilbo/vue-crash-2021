e216635676032a399a0514157c20c9ac9ed0d406
stevenbanilbo <steven.banilbo03@gmail.com>
2022-04-18 20:54:48 +0800

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({ 
 
-  publicPath: '',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-crash-2021/' : '/',
 
   devServer: {
       proxy:{
